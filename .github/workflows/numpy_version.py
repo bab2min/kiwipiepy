7fc2b09cdc23d32f77cdd67f3d451abdb0989835
bab2min <bab2min@gmail.com>
2024-10-03 18:12:28 +0900

    Fix build errors with numpy==2

--- .github/workflows/numpy_version.py
+++ .github/workflows/numpy_version.py
@@ -1,9 +1,13 @@
+import sys
 
-def get_old_numpy_version():
-    import sys
-
+def get_old_numpy_version(use_v1=False):
     py_version = sys.version_info
-    if py_version >= (3, 9): return '2.0.0'
+    if not use_v1 and py_version >= (3, 9):
+        return '2.0.0'
+    if py_version >= (3, 12): return '1.26.0'
+    if py_version >= (3, 11): return '1.24.0'
+    if py_version >= (3, 10): return '1.22.0'
+    if py_version >= (3, 9): return '1.20.0'
     if py_version >= (3, 8): return '1.18.0'
     if py_version >= (3, 7): return '1.15.0'
     if py_version >= (3, 6): return '1.12.0'
@@ -11,4 +15,5 @@ def get_old_numpy_version():
     return '1.10.0'
 
 if __name__ == '__main__':
-    print(get_old_numpy_version())
+    use_v1 = len(sys.argv) > 1 and sys.argv[1] == 'v1'
+    print(get_old_numpy_version(use_v1))
