9625e9793ddfd5a6cf34a51f128fac87559d59f9
bab2min <bab2min@gmail.com>
2024-10-03 17:26:29 +0900

    Add support numpy 2.0 for `python>=3.9`

--- .github/workflows/numpy_version.py
+++ .github/workflows/numpy_version.py
@@ -3,10 +3,7 @@ def get_old_numpy_version():
     import sys
 
     py_version = sys.version_info
-    if py_version >= (3, 12): return '1.26.0'
-    if py_version >= (3, 11): return '1.24.0'
-    if py_version >= (3, 10): return '1.22.0'
-    if py_version >= (3, 9): return '1.20.0'
+    if py_version >= (3, 9): return '2.0.0'
     if py_version >= (3, 8): return '1.18.0'
     if py_version >= (3, 7): return '1.15.0'
     if py_version >= (3, 6): return '1.12.0'
