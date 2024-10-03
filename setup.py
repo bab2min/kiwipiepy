24d568536df3b1c8e28c2c8c58b874d80125e27b
bab2min <bab2min@gmail.com>
2024-10-03 20:15:34 +0900

    Fix `test_offset_mapping` failure

--- setup.py
+++ setup.py
@@ -209,7 +209,7 @@ setup(
     keywords='Korean morphological analysis',
     install_requires=[
         'dataclasses; python_version < "3.7"',
-        'kiwipiepy_model>=0.18,<0.19',
+        'kiwipiepy_model>=0.19,<0.20',
         'numpy<2; python_version < "3.9"',
         'numpy; python_version >= "3.9"',
         'tqdm',
